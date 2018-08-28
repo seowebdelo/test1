5476b22065379f1df9bae3f03f6ad097e95fa453
seowebdelo <40661289+seowebdelo@users.noreply.github.com>
2018-08-28 23:27:04 +0300

    Доработка ВП 1 часть 2

--- vp1/src/form.php
+++ vp1/src/form.php
@@ -14,6 +14,33 @@ if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['tel'])
 else {
     echo "Ошибка: Заполните все поля!";
 }
+Пробую еще этот вариант - но не получается. ТОт код что ниже - тот работает.
+//$sql = "SELECT `id` FROM users WHERE `email` = '$email'";
+//$sth = $pdo->prepare($sql); // подготавливает
+//$sth->execute();
+//$result = $sth->fetch(PDO::FETCH_ASSOC);
+//
+//if ($result === false) {
+//    $sql = "INSERT INTO `users`(`name`, `email`, `tel`) VALUES ('$name' ,'$email','$tel');";
+//    $sth = $pdo->prepare($sql);
+//    $sth->execute();
+//    //почему мы проверяем повторно id после того, как мы записали заказ и где мы добавляем нового пользователя, если он не существует
+//    $sql = "SELECT `id` FROM users WHERE `email` = '$email'";
+//    $sth = $pdo->prepare($sql); // подготавливает
+//    $sth->execute(); //результирующий набор, указатель на результат
+//    $result2 = $sth->fetch(PDO::FETCH_ASSOC); //извлекает. фетч - одномерный массив, одну строку фетч алл - все
+//}
+//
+//$sql = ("INSERT INTO orders (`user_id`,`street`, `home`, `housing`, `flat`, `floor`) VALUES ($result[id], '$street', '$home', '$housing', '$flat', '$floor'))");
+//$sth = $pdo->prepare($sql);
+//$sth->execute();
+//echo '<pre>';
+//
+//print_r($result);
+//print_r($result2);
+
+
+
 $takeEmail = $pdo->query("SELECT  `email` FROM `users` WHERE `email` = '$email'");
 $makeArray = $takeEmail->fetchAll(PDO::FETCH_ASSOC);
 foreach ($makeArray as $key => $value) {
