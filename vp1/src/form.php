cddc05dc87cb32cf85fe376cb915d36e0ea35f8c
seowebdelo <40661289+seowebdelo@users.noreply.github.com>
2018-08-29 15:03:06 +0300

    ПОдправил

--- vp1/src/form.php
+++ vp1/src/form.php
@@ -14,13 +14,13 @@ if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['tel'])
 else {
     echo "Ошибка: Заполните все поля!";
 }
-$sql = "SELECT `id` FROM `users` WHERE `email` = '$email'"; // получаем id по mail который ввели
-$sth = $pdo->prepare($sql); // подготавливает SQL выражение к выполнению
-$sth->execute(); //запускает подготовленный запрос на выполнение
-$result = $sth->fetch(PDO::FETCH_ASSOC); // извлекаем следующую строку
-// если емаил не найден, нужно добавить в базу
+$sql = "SELECT `id` FROM `users` WHERE `email` = '$email'"; // получаем id, если маил в базе
+$sth = $pdo->prepare($sql);
+$sth->execute();
+$result = $sth->fetch(PDO::FETCH_ASSOC);
+// если емаил нет в базе
 if ($result === false){
-    $sql = "INSERT INTO `users`(`name`, `email`, `tel`) VALUES ('$name' ,'$email','$tel');"; // добавление в users
+    $sql = "INSERT INTO `users`(`name`, `email`, `tel`) VALUES ('$name' ,'$email','$tel');"; // вносим в базу
     $sth = $pdo->prepare($sql);
     $sth->execute();
     $sql = "SELECT `id` FROM `users` WHERE `email` = '$email'";
@@ -31,34 +31,27 @@ if ($result === false){
 $sql = "INSERT INTO `orders` (`id_user`, `name`, `street`, `home`, `housing`, `flat`, `floor`, `comment`) VALUES ('$result[id]', '$name', '$street', '$home', '$part', '$appt', '$floor', '$comment');";
 $sth = $pdo->prepare($sql);
 $sth->execute();
-$sql = "SELECT `id` FROM `orders` WHERE `id_user` = '$result[id]'"; //Считаем заказы
+// Считаем заказы
+$sql = "SELECT MAX(`id`) as maxId FROM `orders` WHERE `id_user` = '$result[id]'";
 $getId = $pdo->prepare($sql);
 $getId->execute();
 $resultId = $getId->fetch(PDO::FETCH_ASSOC);
-$sql = "SELECT COUNT(*) as number_orders FROM `orders` WHERE `id_user` = '$result[id]'"; //берем номер заказа
+// берем id заказа
+$sql = "SELECT COUNT(*) as number_orders FROM `orders` WHERE `id_user` = '$result[id]'";
 $countId = $pdo->prepare($sql);
 $countId->execute();
 $resultCountId = $countId->fetch(PDO::FETCH_ASSOC);
 $userOrders = $resultCountId[number_orders];
-if($userOrders == 1) {
-    $sumOrders = 'Ваш первый заказ';
-} else {
-    $sumOrders = 'Спасибо! Это уже ' . $userOrders . ' заказ';
-}
+$sumOrders = 'Это Ваш - ' . $userOrders . ' заказ';
 //создаем форму
 $getTime = date('d.m.Y H.i'); // фиксируем текущее время
 $file = 'basa.html';
-$title =  '<br><br>' . 'Заказ №' . $resultId[id] . '<br>';
+$title =  '<br><br>' . 'Заказ № ' . $resultId[maxId] . '<br>';
 $time = 'Время заказа - ' . $getTime;
-$fullAddress = 'Ваш заказ будет доставлен по адресу - ' . ', ' . $street . ', ' . $home . ', ' . $part . ', ' . $appt . ', ' . $floor . '<br>';
+$fullAddress = 'Ваш заказ будет доставлен по адресу - ' . ', ' . $street . ', ' . $home . ', ' . $housing . ', ' . $flat . ', ' . $floor . '<br>';
 $text = 'DarkBeefBurger за 500 рублей, 1 шт' . '<br>';
-$orderMessage = $title . $time . $fullAddress . $text . $sumOrders;
+echo $orderMessage = $title . $time . $fullAddress . $text . $sumOrders;
 //Работаем с файлом, добавляя запись
 $writeOrder = file_get_contents($file);
 $writeOrder .= $orderMessage;
 file_put_contents($file, $writeOrder);
-
-
-
-
-
