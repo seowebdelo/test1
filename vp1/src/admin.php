812e861f17fe156eae41764667f3d53e48072d0c
seowebdelo <40661289+seowebdelo@users.noreply.github.com>
2018-08-29 16:29:44 +0300

    Доработка списка клиентов и заказов в админ

--- vp1/src/admin.php
+++ vp1/src/admin.php
@@ -1,10 +1,25 @@
 <?php
 $dsn = "mysql:host=localhost;dbname=burger;charset=utf8";
 $pdo = new PDO($dsn, 'root', '');
-?/>
-/**
- * Created by PhpStorm.
- * User: lenovo
- * Date: 28.08.2018
- * Time: 10:28
- */
+$stmt = $pdo->query('SELECT * FROM `users`');
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo "<h2>База пользователей</h2>";
+
+foreach ($result as $key => $value) {
+        echo '<br>';
+    foreach ($value as $k => $v) {
+        echo $k . ' : '  .$v . '<br>';
+    }
+}
+$stmt2 = $pdo->query('SELECT * FROM `orders`');
+$result2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>База заказов</h2>";
+
+foreach ($result2 as $key => $value) {
+    echo '<br>';
+    foreach ($value as $k => $v) {
+        echo $k . ' : '  .$v . '<br>';
+    }
+}
+?>
